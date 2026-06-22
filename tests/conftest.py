a76b47ddd63796253481eb566418877e5e9a8c47
PetrVPA <vpa.interceptor@gmail.com>
2026-06-22 18:13:59 +0300

    Третья домашняя ООП вторая сдача.

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,9 +20,12 @@ def third_product():
 
 @pytest.fixture
 def first_category():
+    third_product = Product('Ракета Р-60', 'Ракета воздух-воздух', 98000.00, 212)
+    second_product = Product('Ракета Р-73Э', 'Ракета воздух-воздух', 117000.00, 112)
     return Category('Ракеты', 'Ракета воздух-воздух', [second_product, third_product])
 
 
 @pytest.fixture
 def second_category():
+    first_product = Product('Ракета Х55', 'Ракета воздух-поверхность', 1256000.00, 26)
     return Category('Ракеты', 'Ракета воздух-поверхность', [first_product])
