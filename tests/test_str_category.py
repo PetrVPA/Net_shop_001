a723029ea00b7cad72657ac8ba39a55b764c81e9
PetrVPA <vpa.interceptor@gmail.com>
2026-06-22 23:05:58 +0300

    Третья домашняя ООП третья сдача.

---
+++ tests/test_str_category.py
@@ -0,0 +1,7 @@
+
+def test_str_category1(first_category):
+    assert str(first_category) == 'Ракеты, количество продуктов: 324 шт.'
+
+
+def test_str_category2(second_category):
+    assert str(second_category) == 'Ракеты, количество продуктов: 26 шт.'
